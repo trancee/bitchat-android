675efafd066d13b989c3e53c0b3a83dbc4e7e250
Philipp Grosswiler <philipp.grosswiler@post.ch>
2026-02-07 15:15:58 +0100

    feat: configure Java toolchains for subprojects in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.jvm.toolchain.JavaToolchainService
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
@@ -9,5 +13,23 @@ plugins {
 tasks.whenTaskAdded {
     if (name.contains("ArtProfile")) {
         enabled = false
+    }
+}
+
+val javaToolchains = extensions.getByType<JavaToolchainService>()
+
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        javaCompiler.set(
+            javaToolchains.compilerFor {
+                languageVersion.set(JavaLanguageVersion.of(8))
+            }
+        )
+    }
+
+    plugins.withId("org.jetbrains.kotlin.android") {
+        extensions.configure<KotlinAndroidProjectExtension> {
+            jvmToolchain(8)
+        }
     }
 }
